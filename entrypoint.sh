14961247a9dd4d6767759a577e6139d614b32636
Mark Murphy <mark.murphy@bigcommerce.com>
2021-08-05 15:16:40 -0500

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 npx @stoplight/spectral@6.0.0-alpha3 lint ./reference/*.yml -f stylish \
   | reviewdog \
       -efm='%-P%f' \
-      -efm=' %#%l:%c-%[0-9]%#:%[0-9]%# %# %trror  %m' \
-      -efm=' %#%l:%c-%[0-9]%#:%[0-9]%# %# %tarning  %m' \
+      -efm=' %#%l:%c %# %trror  %m' \
+      -efm=' %#%l:%c %# %tarning  %m' \
       -efm='%-Q' \
       -efm='%-G%.%#' \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
