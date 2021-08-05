9171994791b2d889223725223d46bc77c143fb28
Mark Murphy <mark.murphy@bigcommerce.com>
2021-08-05 14:28:43 -0500

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ cd "$GITHUB_WORKSPACE" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-npx @stoplight/spectral@6.0.0-alpha3 lint reference/*.yml -v -F=error --ruleset=https://raw.githubusercontent.com/bigcommerce/api-specs/master/.spectral.yaml \
+npx @stoplight/spectral@6.0.0-alpha3 lint ./reference/*.yml -f text --ruleset=https://raw.githubusercontent.com/bigcommerce/api-specs/master/.spectral.yaml \
   | reviewdog \
     -efm="%f:%l:%c: %m" \
     -name "spectral" \
-    -reporter="${INPUT_REPORTER:-github-pr-check}" \
+    -reporter="${INPUT_REPORTER:-github-pr-review}" \
     -level="${INPUT_LEVEL}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
